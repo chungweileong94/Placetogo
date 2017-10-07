e4e7878a478e7ad0d82983d620d0e69ba9cc6515
Chung Wei Leong <leongchungwei@hotmail.com>
2017-10-07 21:00:11 +0800

    Added login page

--- app/src/main/java/com/example/chungwei/placetogo/LoginActivity.java
+++ app/src/main/java/com/example/chungwei/placetogo/LoginActivity.java
@@ -1,7 +1,12 @@
 package com.example.chungwei.placetogo;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewTreeObserver;
+import android.widget.ImageView;
+
+import jp.wasabeef.blurry.Blurry;
 
 public class LoginActivity extends AppCompatActivity {
 
