f816559aba65d41221828f9fd9dd1dcfa908b051
Chung Wei Leong <leongchungwei@hotmail.com>
2017-10-08 00:47:10 +0800

    Touch up UI

--- app/src/main/java/com/example/chungwei/placetogo/MainActivity.java
+++ app/src/main/java/com/example/chungwei/placetogo/MainActivity.java
@@ -1,18 +1,14 @@
 package com.example.chungwei.placetogo;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
@@ -28,7 +24,7 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+        drawer.addDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
@@ -46,92 +42,31 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.addremove) {
-            // Handle the camera action
-        } else if (id == R.id.navigateme) {
-
-        } else if (id == R.id.placevisited) {
-
-        } else if (id == R.id.feelinglucky) {
-
-        } else if (id == R.id.arroundme) {
-
-        } else if (id == R.id.appsettings) {
-
-        } else if (id == R.id.usersettings) {
-
+        switch (item.getItemId()) {
+            case R.id.wishlist_nav_item:
+                break;
+            case R.id.navigate_nav_item:
+                break;
+            case R.id.history_nav_item:
+                break;
+            case R.id.feeling_lucky_nav_item:
+                break;
+            case R.id.near_by_nav_item:
+                break;
+            case R.id.settings_nav_item:
+                break;
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-
-        displaySelectedScreen(item.getItemId());
+        ((DrawerLayout) findViewById(R.id.drawer_layout)).closeDrawer(GravityCompat.START);
 
         return true;
     }
 
-    private void displaySelectedScreen(int itemId) {
-
-        //creating fragment object
-        Fragment fragment = null;
-
-        //initializing the fragment object which is selected
-        switch (itemId) {
-            case R.id.addremove:
-                //fragment = new Menu1();
-                break;
-            case R.id.navigateme:
-                //fragment = new Menu2();
-                break;
-            case R.id.placevisited:
-                //fragment = new Menu3();
-                break;
-            case R.id.feelinglucky:
-                //fragment = new Menu1();
-                break;
-            case R.id.arroundme:
-                //fragment = new Menu2();
-                break;
-            case R.id.appsettings:
-                //fragment = new Menu3();
-                break;
-        }
-
-        //replacing the fragment
-        if (fragment != null) {
-            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-            ft.replace(R.id.mainContainer, fragment);
-            ft.commit();
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
+    private void navigateFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.content_frameLayout, fragment)
+                .commit();
     }
 }
