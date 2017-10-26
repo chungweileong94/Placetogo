12ea616ffabcf0aae47f21f25492ed1e345a0e9a
Tian Xiang Tee <developertx88@gmail.com>
2017-10-26 22:17:32 +0800

    TTS added ! Hooarraaaay

---
+++ app/src/main/java/com/example/chungwei/placetogo/services/TTS.java
@@ -0,0 +1,31 @@
+package com.example.chungwei.placetogo.services;
+
+import android.content.Context;
+import android.speech.tts.TextToSpeech;
+
+/**
+ * Created by Tian Xiang Tee on 26/10/2017.
+ */
+
+public class TTS {
+
+    private static TextToSpeech textToSpeech;
+
+    public static void init(final Context context) {
+        if (textToSpeech == null) {
+            textToSpeech = new TextToSpeech(context, new TextToSpeech.OnInitListener() {
+                @Override
+                public void onInit(int i) {
+
+                }
+            });
+        }
+    }
+
+    public static void speak(final String text) {
+        textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+    }
+
+    }
+
+
